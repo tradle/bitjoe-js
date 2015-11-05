14f7096665cb67d05abebc2fa827033e13f42a36
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-11-05 17:46:11 +0000

    simplify some test setup

--- test/common.js
+++ test/common.js
@@ -5,10 +5,11 @@ var helpers = require('tradle-test-helpers')
 var fakeKeeper = helpers.fakeKeeper
 var fakeWallet = helpers.fakeWallet
 var ChainLoader = require('chainloader')
-var mi = require('midentity')
-var Identity = mi.Identity
-var AddressBook = mi.AddressBook
-var Keys = mi.Keys
+// var mi = require('midentity')
+// var Identity = mi.Identity
+// var AddressBook = mi.AddressBook
+// var kiki = require('kiki')
+// var Keys = kiki.Keys
 
 var common = module.exports = {
   nulls: function (size) {
@@ -52,57 +53,82 @@ var common = module.exports = {
       minConf: 0
     })
   },
-  identityFor: function (key, networkName) {
-    var priv, pub
-    if (key.pub) {
-      priv = key
-      pub = key.pub
-    } else {
-      pub = key
+  // identityFor: function (key, networkName) {
+  //   var priv, pub
+  //   if (key.pub) {
+  //     priv = key
+  //     pub = key.pub
+  //   } else {
+  //     pub = key
+  //   }
+
+  //   networkName = networkName || 'testnet'
+  //   var i = new Identity()
+  //     .addKey(new Keys.Bitcoin({
+  //       priv: priv,
+  //       pub: pub,
+  //       networkName: networkName,
+  //       purpose: 'file-sharing'
+  //     }))
+
+  //   mi.defaultKeySet({
+  //     networkName: networkName
+  //   }).forEach(i.addKey, i)
+
+  //   return i
+  // },
+  chainloaderFor: function (joe, friends) {
+    var networkName = joe.config('networkName')
+    var network = bitcoin.networks[networkName]
+    var addressBook = {}
+    var priv = joe.wallet().priv
+    var joePriv = {
+      type: 'bitcoin',
+      network: networkName,
+      priv: priv.toWIF(network),
+      value: priv.pub.toHex(),
+      fingerprint: priv.pub.getAddress(network).toString(),
+      purpose: 'messaging'
     }
 
-    return new Identity()
-      .addKey(new Keys.Bitcoin({
-        priv: priv,
-        pub: pub,
-        networkName: networkName || 'testnet',
-        purpose: 'file-sharing'
-      }))
-  },
-  chainloaderFor: function (joe, friends) {
-    var net = joe.config('networkName')
-    var addressBook
     if (friends) {
-      addressBook = new AddressBook()
-      friends.forEach(function (pk) {
-        addressBook.add(common.identityFor(bitcoin.ECKey.fromWIF(pk)))
+      friends.forEach(function (f) {
+        var key = bitcoin.ECKey.fromWIF(f)
+        var pub = {
+          type: 'bitcoin',
+          network: networkName,
+          value: key.pub.toHex(),
+          fingerprint: key.pub.getAddress(network).toString(),
+          purpose: 'messaging'
+        }
+
+        addressBook[pub.fingerprint] = pub
       })
     }
 
-    var joeIdent = common.identityFor(joe.wallet().priv, net)
-    var pubs = joeIdent.exportKeys()
-    var privs = joeIdent.exportKeys(true)
+    // var joeIdent = common.identityFor(joe.wallet().priv, net)
+    // var pubs = joeIdent.exportKeys()
+    // var privs = joeIdent.exportKeys(true)
 
     return new ChainLoader({
       wallet: joe.wallet(),
       keeper: joe.keeper(),
-      networkName: net,
+      networkName: networkName,
       prefix: joe.config('prefix'),
       lookup: function (addr, returnPrivate) {
         if (addr === joe.wallet().addressString) {
-          var keys = returnPrivate ? privs : pubs
           return Q.resolve({
-            key: keys.filter(function (k) {return k.fingerprint === addr})[0],
-            identity: joeIdent
+            key: joePriv
           })
         }
 
-        return Q.resolve(addressBook.byFingerprint(addr))
-          .then(function (result) {
-            result.identity = result.identity.toJSON()
-            result.key = result.key.toJSON()
-            return result
+        if (addressBook[addr]) {
+          return Q.resolve({
+            key: addressBook[addr]
           })
+        } else {
+          return Q.reject(new Error('not found'))
+        }
       }
     })
   }
